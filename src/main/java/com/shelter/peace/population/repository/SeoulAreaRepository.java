be880ad1fcbd3b66342deeb7067fd581425c20b8
eseulssi <eeeeeseulllll@naver.com>
2023-10-10 18:24:01 +0900

    feat: population api 구현 및 서울 113곳 엑셀 데이터 저장

---
+++ src/main/java/com/shelter/peace/population/repository/SeoulAreaRepository.java
@@ -0,0 +1,7 @@
+package com.shelter.peace.population.repository;
+
+import com.shelter.peace.population.entity.SeoulArea;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeoulAreaRepository extends JpaRepository<SeoulArea, Long> {
+}
