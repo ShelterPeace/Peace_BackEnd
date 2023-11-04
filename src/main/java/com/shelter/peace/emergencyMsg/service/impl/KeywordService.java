b1a10460a6fb1ba00555cb24b525af9d056ce7b5
Youngeunlee98 <helen613@naver.com>
2023-11-04 12:19:50 +0900

    BIG-55: 재난문자분류 수정중

--- src/main/java/com/shelter/peace/emergencyMsg/service/impl/KeywordService.java
+++ src/main/java/com/shelter/peace/emergencyMsg/service/impl/KeywordService.java
@@ -2,39 +2,94 @@ package com.shelter.peace.emergencyMsg.service.impl;
 
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class KeywordService {
-    private Map<String, String> keywordToSentence;
+    private Map<String, List<String>> keywordToSentences;
 
     public KeywordService() {
-        keywordToSentence = new HashMap<>();
-        keywordToSentence.put("산사태", "산사태에 관한 안내사항 입니다.");
-        keywordToSentence.put("폭우", "폭우에 관한 안내사항 입니다.");
-        keywordToSentence.put("태풍", "태풍에 관한 안내사항 입니다.");
-        keywordToSentence.put("실종", "실종에 관한 안내사항 입니다.");
-        keywordToSentence.put("호우", "호우에 관한 안내사항 입니다.");
-        keywordToSentence.put("지진", "지진에 관한 안내사항 입니다.");
-        keywordToSentence.put("해일", "해일에 관한 안내사항 입니다.");
-        keywordToSentence.put("교통", "교통에 관한 안내사항 입니다.");
-        keywordToSentence.put("폭염", "폭염에 관한 안내사항 입니다.");
-        keywordToSentence.put("강풍", "강풍에 관한 안내사항 입니다.");
-        // 다른 키워드와 문장 추가
+        keywordToSentences = new HashMap<>();
+
+        //대표키워드 : 실종
+        List<String> aKeywords = new ArrayList<>();
+        aKeywords.add("목격");
+        aKeywords.add("배회");
+        // "실종"에 대한 문장
+        List<String> aSentences = new ArrayList<>();
+        aSentences.add("실종 관련 안내사항");
+        keywordToSentences.put("실종", aKeywords);
+        keywordToSentences.put("실종", aSentences);
+
+        //대표키워드 : 교통
+        List<String> bKeywords = new ArrayList<>();
+        bKeywords.add("운전");
+        bKeywords.add("차량");
+        // "교통"에 대한 문장
+        List<String> bSentences = new ArrayList<>();
+        bSentences.add("교통 관련 안내사항");
+        keywordToSentences.put("교통", bKeywords);
+        keywordToSentences.put("교통", bSentences);
+
+        //대표키워드 : 화재
+        List<String> cKeywords = new ArrayList<>();
+        cKeywords.add("산불");
+        // "화재"에 대한 문장
+        List<String> cSentences = new ArrayList<>();
+        cSentences.add("화재 관련 안내사항");
+        keywordToSentences.put("화재", cKeywords);
+        keywordToSentences.put("화재", cSentences);
+
+        //대표키워드 : 지진
+        List<String> dKeywords = new ArrayList<>();
+        dKeywords.add("여진발생");
+        // "지진"에 대한 문장
+        List<String> dSentences = new ArrayList<>();
+        dSentences.add("지진 관련 안내사항");
+        keywordToSentences.put("지진", dKeywords);
+        keywordToSentences.put("지진", dSentences);
+
+        //대표키워드 : 태풍
+        List<String> eKeywords = new ArrayList<>();
+        eKeywords.add("강풍");
+        eKeywords.add("폭풍");
+        // "태풍"에 대한 문장
+        List<String> eSentences = new ArrayList<>();
+        eSentences.add("태풍 및 강풍 관련 안내사항");
+        keywordToSentences.put("태풍", eKeywords);
+        keywordToSentences.put("태풍", eSentences);
+
+        //대표키워드 : 호우
+        List<String> fKeywords = new ArrayList<>();
+        fKeywords.add("비");
+        fKeywords.add("침수");
+        fKeywords.add("홍수");
+        // "교통"에 대한 문장
+        List<String> fSentences = new ArrayList<>();
+        cSentences.add("호우 및 비 관련 안내사항");
+        keywordToSentences.put("호우", fKeywords);
+        keywordToSentences.put("호우", fSentences);
+
     }
 
     public String processMessageForKeywords(String message) {
-        for (Map.Entry<String, String> entry : keywordToSentence.entrySet()) {
-            String keyword = entry.getKey();
-            String sentence = entry.getValue();
+        for (Map.Entry<String, List<String>> entry : keywordToSentences.entrySet()) {
+            String representativeKeyword = entry.getKey();
+            List<String> relatedKeywords = entry.getValue();
 
-            if (message.contains(keyword)) {
-                return sentence;
+            if (message.contains(representativeKeyword) || relatedKeywords.stream().anyMatch(message::contains)) {
+                return getKeywordSentences(representativeKeyword);
             }
         }
-        // 키워드가 없는 경우 처리
-        return "설정된 키워드에 해당하지 않습니다.";
+        return ""; // 아무 내용도 반환하지 않음
+    }
+
+    private String getKeywordSentences(String keyword) {
+        List<String> sentences = keywordToSentences.get(keyword);
+        if (sentences != null) {
+            return String.join("\n", sentences);
+        }
+        return "";
     }
 
 }
