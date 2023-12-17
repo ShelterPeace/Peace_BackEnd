6bd3924ce1849f1081647576a4efdc540a28c4f2
minje <alswp0910@naver.com>
2023-12-17 22:34:22 +0900

    배포 테스트

--- src/main/java/com/shelter/peace/PeaceApplication.java
+++ src/main/java/com/shelter/peace/PeaceApplication.java
@@ -2,14 +2,21 @@ package com.shelter.peace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class PeaceApplication {
+public class PeaceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PeaceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PeaceApplication.class);
     }
 
 }
